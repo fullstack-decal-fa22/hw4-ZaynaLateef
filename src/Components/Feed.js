6289406a423ee42aa613230c5e3fcdc0c23e0b7f
zayna lateef <zaynalateef@SAMINAs-MacBook-Air.local>
2022-10-31 23:46:32 -0700

    final

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import React from 'react';
 import Menu from './Menu';
-import Block from './Block';
 
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
+    const [p] = useState([]);
 
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const posts = p.map((item) => (<li> (item) </li>)); // TODO: edit this variable
 
     return (
         <div>
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Color from './Color';
+import Block from './Block';
+import { useState } from 'react';
 /* Add any imports you think you might need here! */
 
 const Menu = () => { 
-
+  const [p, setP] = useState([]);
+  function clicked(color2) {
+    setP(block => [<Block color = {color2}/>, ...block]);
+  }
+  const posts = p.map((il) => (il));
     return (
-      <div className="colorOptions">
+      <div> 
+        <div className="colorOptions">
           {/* TODO */}
+          <Color color="purple" handleClick = {clicked}/>
+          <Color color="pink" handleClick = {clicked}/>
+          <Color color="blue" handleClick = {clicked}/>
+          <Color color="orange" handleClick = {clicked}/>
+        </div>
+        {posts}
       </div>
     );
 }
